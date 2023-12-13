40aa1d3752b2c2cd2e28d8c43636002dbd7ff70d
DANI <dcamargo@prueba.com>
2023-12-13 09:17:06 +0100

    eliminar archivos

--- ejercicios/papelera.js
+++
@@ -1,7 +0,0 @@
-
-    document.querySelector(".trash").addEventListener('click', function()
-    {
-        document.querySelector(".trash").style.backgroundImage="none";
-        document.querySelector(".trash").style.backgroundImage="URL('https://cdn.icon-icons.com/icons2/12/PNG/256/recycling_recyclebin_empty_delete_trash_1771.png')";
-        setTimeout(()=>{alert("Papelera borrada con exito")},40);
-    });
